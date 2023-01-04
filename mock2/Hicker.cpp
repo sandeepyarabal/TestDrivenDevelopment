70333fc7c85c9b832d7484e8ed4853d52b7ee53a
Sandeep Patil <sandeep.yarabal@gmail.com>
2023-01-03 23:59:23 -0500

    Create Hicker.cpp

---
+++ mock2/Hicker.cpp
@@ -0,0 +1,17 @@
+#include "Hiker.hpp"
+void productionCode(int ,int);
+Hiker::Hiker()
+{
+}
+
+Hiker::~Hiker()
+{
+}
+
+int Hiker::answer() const
+{
+    int a =3;
+    int b = 3;
+    productionCode(a,b);
+    return 6 * 9;
+}
